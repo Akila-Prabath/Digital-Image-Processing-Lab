b4e3f326ae7bbd40b8a32547ac6df2872cdda716
Akila Prabath <akilaprabath861@gmail.com>
2026-01-28 08:16:00 +0530

    create rgb to gray file

--- 01_basics/rgb_to_gray.py
+++ 01_basics/rgb_to_gray.py
@@ -4,20 +4,23 @@ import matplotlib.pyplot as plt
 # read image
 img = cv2.imread("../Images/flower.jpg")
 
-# convert image color from BGR to RGB
+# BGR to RGB
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# show images
+# BGR to gray
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# show image
 plt.figure(figsize=(10,4))
 
 plt.subplot(1,2,1)
-plt.imshow(img)
-plt.title("BGR Image")
+plt.imshow(img_rgb)
+plt.title("Original Image")
 plt.axis("off")
 
 plt.subplot(1,2,2)
-plt.imshow(img_rgb)
-plt.title("Original Image")
+plt.imshow(img_gray, cmap="gray")
+plt.title("Gray Image")
 plt.axis("off")
 
 plt.show()
