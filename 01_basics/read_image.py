56b8eb3372118dba909d0e445e8cf041eaad5304
Akila Prabath <akilaprabath861@gmail.com>
2026-01-28 07:51:59 +0530

    Update read_image.py

--- 01_basics/read_image.py
+++ 01_basics/read_image.py
@@ -7,8 +7,17 @@ img = cv2.imread("../Images/flower.jpg")
 # convert image color from BRG to RGB
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# show image
+# show images
+plt.figure(figsize=(10,4))
+
+plt.subplot(1,2,1)
+plt.imshow(img)
+plt.title("BRG Image")
+plt.axis("off")
+
+plt.subplot(1,2,2)
 plt.imshow(img_rgb)
 plt.title("Original Image")
 plt.axis("off")
+
 plt.show()
